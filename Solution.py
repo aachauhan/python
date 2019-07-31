1d87679d6bf7aa36d1ed531b8d744ddabf3d6b94
Aditya Chauhan <adie@nuethic.com>
2019-07-31 17:42:47 -0600

    Leetcode Problem 7 solution

--- Solution.py
+++ Solution.py
@@ -1,24 +1,26 @@
 class Solution:
-    def __init__(self):
-        print("in init")
-
     def reverse(self, x: int) -> int:
         s = str(x)
         result = list()
         count = len(s)
         index = 0
 
-        if (s[0] != '-'):
+        # new_index = max(0, min(new_index, len(mylist)-1))
+
+        # new_index = max(0, min(new_index, len(mylist)-1))
+
+        if s[0] != '-':
             for i in range(len(s) - 1, -1, -1):
-                # print(s[i])
                 result.append(s[i])
         else:
             result.insert(0, '-')
             for i in range(len(s) - 1, 0, -1):
-                # print(s[i])
                 result.append(s[i])
 
         tempStr = [str(m) for m in result]
         res = int("".join(map(str, tempStr)))
 
-        return (res)
+        if res > 2147483647 or res < -2147483647:
+            return 0
+        else:
+            return res
